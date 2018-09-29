fa3336e73bbbd7914d3ea7c58925c21bad4b372e
BoBeenLee <cultist_tp@naver.com>
2018-09-29 15:33:28 +0900

    불필요 부분 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,19 +17,6 @@ module.exports = {
       }
     },
 
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true
-      }
-    },
-
     // Parse all markdown files (each plugin add/parse some data into graphQL layer)
     {
       resolve: `gatsby-transformer-remark`,
@@ -74,13 +61,6 @@ module.exports = {
         display: `minimal-ui`
       }
     },
-    /* eslint-enable camelcase */
-
-    // This plugin generates a service worker and AppShell
-    // html file so the site works offline and is otherwise
-    // resistant to bad networks. Works with almost any
-    // site!
-    `gatsby-plugin-offline`,
     `gatsby-plugin-styled-components`
   ]
 };
